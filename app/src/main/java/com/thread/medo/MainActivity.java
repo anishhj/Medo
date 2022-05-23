20cefd03827568fc2bd8ebcabca336f5f3a96463
sidd2203 <siddharthch22@gmail.com>
2022-05-23 17:47:27 +0530

    initial setup

---
+++ app/src/main/java/com/thread/medo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.thread.medo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

c3bbbbb8e8acdc1b245630a5b6b4483dcac71827
Ayush <ayushjaindeoli01@gmail.com>
2022-05-26 23:00:55 +0530

    Update Activity

---
+++ app/src/main/java/com/thread/medo/update.java
@@ -0,0 +1,13 @@
+package com.thread.medo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}

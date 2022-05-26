e3d0afcbf09e394183f2c5568015b987300b8c3f
Ayush <ayushjaindeoli01@gmail.com>
2022-05-26 23:30:39 +0530

    upload activity

---
+++ app/src/main/java/com/thread/medo/upload.java
@@ -0,0 +1,14 @@
+package com.thread.medo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class upload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}

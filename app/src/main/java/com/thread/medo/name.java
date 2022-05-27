03a729e6b1e3e2970881e015d60c5725e2001c0d
Ayush <ayushjaindeoli01@gmail.com>
2022-05-28 00:14:09 +0530

    Name Activity

---
+++ app/src/main/java/com/thread/medo/name.java
@@ -0,0 +1,14 @@
+package com.thread.medo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class name extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+    }
+}
